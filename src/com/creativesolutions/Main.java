fc1e0ecd3ffea51a4fc496add81ade5f8ce15719
jameswood <james_s_wood@outlook.com>
2021-03-01 01:13:23 -0500

    Adding files in project 'pussy_cat' to Git...

---
+++ src/com/creativesolutions/Main.java
@@ -0,0 +1,9 @@
+package com.creativesolutions;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Pussy Cat!");
+        System.out.println("How are you today?");
+    }
+}
